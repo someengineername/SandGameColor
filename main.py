22a088be04caf1e7890136baee3a76fe4f85aaec
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-05-19 20:03:53 +0400

    class introduction: moving dot and empty space - base for further progress simple drawing engine implemented (based on object type)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame
 import time
 import random
-
-print('hello!')
+import functools
 
 pygame.init()
 
@@ -16,22 +15,51 @@ def tick(timing: int | float):
 # screen initialization with update to cell size
 screen_width = 800
 screen_height = 600
+screen_base_color = (0, 0, 0)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # main loop
 run = True
 
+# mouse interaction variables
 GLOBAL_MOUSE_PRESSED = False
+
+
+# class of pixel - to draw with some
+class MovingDot:
+    def __init__(self, int1=50, int2=50, int3=50):
+        self._color = (int1, int2, int3)
+
+    def get_color(self):
+        return self._color
+
+
+class EmptySpace:
+    def __init__(self):
+        self._color = (20, 20, 20)
+
+    def get_color(self):
+        return self._color
+
+
+# matrix preparation
+matrix_dimension_y = 10
+matrix_dimension_x = 15
+cell_length = 25
+cell_gap = 2
+matrix = [[EmptySpace() for j in range(matrix_dimension_y)] for i in range(matrix_dimension_x)]
+
+matrix[5][5] = MovingDot()
 
 while run:
 
-    screen.fill((0, 0, 0))
-
     for event in pygame.event.get():
 
-        # different variations of events goes here...
+        # window closing event
         if event.type == pygame.QUIT:
             run = False
+
+        # mouse interaction event
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             GLOBAL_MOUSE_PRESSED = True
@@ -39,11 +67,34 @@ while run:
         if event.type == pygame.MOUSEBUTTONUP:
             GLOBAL_MOUSE_PRESSED = False
 
+    # ---------
+    # drawing section
+    # ---------
+
+    # screen fill
+    screen.fill(screen_base_color)
+
+    # draw base matrix
+    for i in range(matrix_dimension_x):
+        for j in range(matrix_dimension_y):
+
+            if isinstance(matrix[i][j], MovingDot):
+                pygame.draw.rect(screen, matrix[i][j].get_color(), (i * (cell_length + cell_gap) + cell_gap,
+                                                                    j * (cell_length + cell_gap) + cell_gap,
+                                                                    cell_length, cell_length))
+            else:
+                pygame.draw.rect(screen, matrix[i][j].get_color(), (i * (cell_length + cell_gap) + cell_gap,
+                                                                    j * (cell_length + cell_gap) + cell_gap,
+                                                                    cell_length, cell_length))
+
     if GLOBAL_MOUSE_PRESSED:
         pygame.draw.rect(screen, (255, 255, 255), (100, 100, 100, 100))
 
+    if pygame.mouse.get_focused():
+        pygame.draw.rect(screen, (150, 150, 150), (10, 10, 10, 10))
+
     # -------------
-    # frame-generation-like
+    # frame generation
     # -------------
     tick(0.0001)
 
