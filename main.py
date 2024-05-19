ca647466b68e3978bcfc0dc04ff74ea4ae9827ad
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-05-19 23:51:20 +0400

    refactoring window adjustment to matrix simple falling meachnism coordinate system ACCURATE AND CORRECT debug-drawing update

--- main.py
+++ main.py
@@ -6,26 +6,6 @@ import functools
 pygame.init()
 
 
-# temp function of tick (time-control)
-def tick(timing: int | float):
-    time.sleep(timing)
-    pygame.display.update()
-
-
-# screen initialization with update to cell size
-screen_width = 800
-screen_height = 600
-screen_base_color = (0, 0, 0)
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-# main loop
-run = True
-
-# mouse interaction variables
-GLOBAL_MOUSE_PRESSED = False
-
-
-# class of pixel - to draw with some
 class MovingDot:
     def __init__(self, int1=50, int2=50, int3=50):
         self._color = (int1, int2, int3)
@@ -42,14 +22,38 @@ class EmptySpace:
         return self._color
 
 
+# temp function of tick (time-control)
+def tick(timing: int | float):
+    time.sleep(timing)
+    pygame.display.update()
+
+
 # matrix preparation
 matrix_dimension_y = 10
 matrix_dimension_x = 15
 cell_length = 25
 cell_gap = 2
-matrix = [[EmptySpace() for j in range(matrix_dimension_y)] for i in range(matrix_dimension_x)]
+matrix = [[EmptySpace() for j in range(matrix_dimension_x)] for i in range(matrix_dimension_y)]
 
-matrix[5][5] = MovingDot()
+# screen initialization with update to cell size
+screen_width = (cell_gap + cell_length) * matrix_dimension_x + cell_gap
+screen_height = (cell_gap + cell_length) * matrix_dimension_y + cell_gap
+screen_base_color = (0, 0, 0)
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# main loop
+run = True
+
+# mouse interaction variables
+GLOBAL_MOUSE_PRESSED = False
+
+# class of pixel - to draw with some
+
+
+# matrix[5][5] = MovingDot()
+# matrix[0][0] = MovingDot()
+# matrix[10][13] =  MovingDot()
+# matrix[19][34] = MovingDot()
 
 while run:
 
@@ -62,6 +66,17 @@ while run:
         # mouse interaction event
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+
+            # pressing-nesting of Moving|Empty spaces
+            mouse_coor = pygame.mouse.get_pos()
+            mouse_matrix_position = [mouse_coor[0] // (cell_length + cell_gap),
+                                     (screen_height - mouse_coor[1]) // (cell_length + cell_gap)]
+
+            if isinstance(matrix[mouse_matrix_position[1]][mouse_matrix_position[0]], MovingDot):
+                matrix[mouse_matrix_position[1]][mouse_matrix_position[0]] = EmptySpace()
+            else:
+                matrix[mouse_matrix_position[1]][mouse_matrix_position[0]] = MovingDot()
+
             GLOBAL_MOUSE_PRESSED = True
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -74,21 +89,30 @@ while run:
     # screen fill
     screen.fill(screen_base_color)
 
-    # draw base matrix
-    for i in range(matrix_dimension_x):
-        for j in range(matrix_dimension_y):
+    # nesting second matrix (for falling process)
+
+    second_matrix = [[EmptySpace() for j in range(matrix_dimension_x)] for i in range(matrix_dimension_y)]
+
+    for i in range(matrix_dimension_y):
+        for j in range(matrix_dimension_x):
 
             if isinstance(matrix[i][j], MovingDot):
-                pygame.draw.rect(screen, matrix[i][j].get_color(), (i * (cell_length + cell_gap) + cell_gap,
-                                                                    j * (cell_length + cell_gap) + cell_gap,
-                                                                    cell_length, cell_length))
-            else:
-                pygame.draw.rect(screen, matrix[i][j].get_color(), (i * (cell_length + cell_gap) + cell_gap,
-                                                                    j * (cell_length + cell_gap) + cell_gap,
-                                                                    cell_length, cell_length))
+
+                if i != 0:
+                    matrix[i][j] = EmptySpace()
+                    matrix[i - 1][j] = MovingDot()
+
+    # drawing matrix
+    for i in range(matrix_dimension_y):
+        for j in range(matrix_dimension_x):
+            pygame.draw.rect(screen, matrix[i][j].get_color(), (cell_gap + ((cell_length + cell_gap) * j),
+                                                                screen_height - cell_gap - cell_length - (
+                                                                            cell_gap + cell_length) * i,
+                                                                cell_length,
+                                                                cell_length))
 
     if GLOBAL_MOUSE_PRESSED:
-        pygame.draw.rect(screen, (255, 255, 255), (100, 100, 100, 100))
+        pygame.draw.rect(screen, (150, 150, 150), (10, 30, 10, 10))
 
     if pygame.mouse.get_focused():
         pygame.draw.rect(screen, (150, 150, 150), (10, 10, 10, 10))
@@ -96,6 +120,6 @@ while run:
     # -------------
     # frame generation
     # -------------
-    tick(0.0001)
+    tick(0.1)
 
 pygame.quit()
